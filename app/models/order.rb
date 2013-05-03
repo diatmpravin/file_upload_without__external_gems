b05fef0fa5febbf6301d9545562ffb719d72e738
PravinKShepHertz <pravin.kumar@shephertz.co.in>
2013-05-03 17:42:04 +0530

    initial commit

---
+++ app/models/order.rb
@@ -0,0 +1,3 @@
+class Order < ActiveRecord::Base
+  attr_accessible :my_file
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FileUploadWithoutExternalGems::Application.config.secret_token = '52735c21181ec695d6291a53a50fc3d3fecf7e245f397154de3c5963ef0c5d0715648158f573054e6a532be933ac66e20bd2e21373ac05d8e9a597eb519a59ed'
